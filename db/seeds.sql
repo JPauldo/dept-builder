18cc0f59b344f048bccb412cf85f95dd03e60f5a
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-15 21:13:31 -0500

    Updated seeds file and added a file to the .gitignore

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,34 +1,46 @@
 INSERT INTO department (name)
      VALUES ("Kanto"),
-            ("Johto");
+            ("Johto"),
+            ("Hoenn"),
+            ("Sinnoh"),
+            ("Unova");
 
 INSERT INTO role (title, salary, department_id)
-     VALUES ("Gym Leader", 30690.00, 1),
-            ("Elite Four", 58410.00, 1),
-            ("Champion", 64350.00, 1),
-            ("Gym Leader", 25500.00, 2),
-            ("Elite Four", 44750.00, 2),
-            ("Champion", 50000.00, 2);
+     VALUES ("Rocket Grunt", 30690.00, 1),
+            ("Rocket Executive", 67500.00, 1),
+            ("Rocket Boss", 82800.00, 1),
+            ("Elite Four", 76800.00, 2),
+            ("Champion", 150000.00, 2),
+            ("Dragon Tamer", 88000.00, 3),
+            ("Dragon Master", 120000.00, 3),
+            ("Ace Trainer", 52800.00, 4),
+            ("Diamond Teacher", 89280.00, 4),
+            ("Beauty", 89100.00, 5),
+            ("Shining Model", 115200.00, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-     VALUES ("Blue", "Oak", 1, NULL),
-            ("Brock", "Nibi", 1, 1),
-            ("Misty", "Hanada", 1, 1),
-            ("Surge", "Kuchiba", 1, 1),
-            ("Erika", "Tamamushi", 1, 1),
-            ("Koga", "Sekichiku", 1, 1),
-            ("Sabrina", "Yamabuki", 1, 1),
-            ("Blaine", "Guren", 1, 1),
-            ("Giovanni", "Tokiwa", 1, 1),
-            ("Lorelei", "Prima", 1, 1),
-            ("Bruno", "Takenori", 1, 1),
-            ("Agatha", "Cion", 1, 1),
-            ("Lance", "Fusube", 1, 1),
-            ("Red", "Masara", 1, NULL),
-            ("Falkner", "Kikyo", 1, 14),
-            ("Bugsy", "Hiwada", 1, 14),
-            ("Will", "Natio", 1, 14),
-            ("Karen", "Blacki", 1, 14);
+     VALUES ("Giovanni", "Sakaki", 3, NULL),
+            ("Lance", "Proton", 2, 1),
+            ("Lambda", "Petrel", 2, 1),
+            ("Athena", "Ariana", 2, 1),
+            ("Apollo", "Archer", 2, 1),
+            ("Jessie", "Mushashi", 1, 1),
+            ("James", "Kojiro", 1, 1),
+            ("Lance", "Fusube", 5, NULL),
+            ("Will", "Natio", 4, 8),
+            ("Karen", "Blacki", 4, 8),
+            ("Drake", "Bohmander", 7, NULL),
+            ("Aaron", "Kairyu", 6, 11),
+            ("Dray", "Tyltalis", 6, 11),
+            ("Egon", "Hyporoi", 6, 11),
+            ("Nicolas", "Libegon", 6, 11),
+            ("Candice", "Kissaki", 9, NULL),
+            ("Sergio", "Nyula", 8, 16),
+            ("Brenna", "Yukikaburi", 8, 16),
+            ("Elesa", "Raimon", 11, NULL),
+            ("Nikola", "Mokoko", 10, 19),
+            ("Fleming", "Shimama", 10, 19),
+            ("Ampere", "Elekid", 10, 19);
 
 SELECT *
   FROM department;
